e145f7b063821c02de7b60b84eae984a39355223
Putra Prasetya <putraprasetya333@gmail.com>
2023-06-27 17:08:27 +0700

    Menambahkan Model Komik dan View nya

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,6 +34,9 @@ $routes->set404Override();
 $routes->get('/', 'Pages::index');
 $routes->get('/about', 'Pages::about');
 $routes->get('/contact', 'Pages::contact');
+$routes->get('/komik', 'Komik::index');
+
+$routes->get('/komik/(:any)', 'Komik::detail/$1');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
